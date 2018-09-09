fac484937e3f7a0754547af5b7b153456b85950e
Suzy <susan.m.mullin@gmail.com>
2018-09-09 14:49:43 -0700

    Checkpoint 7 of Bloc Jams React: Completed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import albumData from './../data/albums';
+import PlayerBar from './PlayerBar';
 
 class Album extends Component {
   constructor(props) {
@@ -46,14 +47,14 @@ class Album extends Component {
     }
   }
 
-  play() {
-    this.audioElement.play();
-    this.setState({ isPlaying: true });
-  }
-
-  pause() {
-    this.audioElement.pause();
-    this.setState({ isPlaying: false });
+  handlePrevClick() {
+    const currentIndex = this.state.album.songs.findIndex(
+      song => this.state.currentSong === song
+    );
+    const newIndex = Math.max(0, currentIndex - 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
   }
 
   handleAudioBtnIcon(song, index) {
@@ -108,6 +109,12 @@ class Album extends Component {
             ))}
           </tbody>
         </table>
+        <PlayerBar
+          isPlaying={this.state.isPlaying}
+          currentSong={this.state.currentSong}
+          handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+          handlePrevClick={() => this.handlePrevClick()}
+        />
       </section>
     );
   }
